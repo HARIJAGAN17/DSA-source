533e6c82494eb003f56f8af5bc484773bc37c727
FantasWolf <jaganhari2002@gmail.com>
2024-04-23 18:25:23 +0530

    FloodFill

---
+++ FloodFill.java
@@ -0,0 +1,48 @@
+package ust;
+
+class FloodFill {
+
+    public void dfs(int[][] grid,int sr,int sc,int newcolor){
+
+            int oldcolor = grid[sr][sc];
+            if(oldcolor==newcolor){
+                return;
+            }
+            int n = grid.length;
+            int m = grid[0].length;
+
+            Stack<int[]> stack= new Stack<>();
+            stack.add(new int[]{sr,sc});
+
+            while(!stack.isEmpty()){
+                int current[] = stack.pop();
+                int cr = current[0];
+                int cc = current[1];
+                grid[cr][cc] = newcolor;
+
+                for(int delrow=-1;delrow<=1;delrow++){
+                    for(int delcol=-1;delcol<=1;delcol++){
+
+                        if(Math.abs(delrow)+Math.abs(delcol)!=1){
+                            continue;
+                        }
+                        int nrow = cr+delrow;
+                        int ncol = cc+delcol;
+                        if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol]==oldcolor){
+                            stack.add(new int[]{nrow,ncol});
+                        }
+                    }
+                }
+
+            }
+            
+
+    }
+
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        
+        int ans[][] = image;
+        dfs(ans,sr,sc,color);
+        return ans;
+    }
+}
