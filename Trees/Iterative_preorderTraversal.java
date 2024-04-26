f7051a33f1d2ab5ff99b64d5a930aff31f7d4eee
FantasWolf <jaganhari2002@gmail.com>
2024-04-26 18:49:16 +0530

    Iterative_preorderTraversal++

--- Trees/Iterative_preorderTraversal.java
+++ Trees/Iterative_preorderTraversal.java
@@ -1,5 +1,50 @@
 package ust.Trees;
-
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
 public class Iterative_preorderTraversal {
 
+}
+
+/**
+ * Definition for a binary tree node.
+ *
+ */
+class TreeNode {
+  int val;
+     TreeNode left;
+     TreeNode right;
+     TreeNode() {}
+     TreeNode(int val) { this.val = val; }
+     TreeNode(int val, TreeNode left, TreeNode right) {
+         this.val = val;
+         this.left = left;
+         this.right = right;
+     }
+ }
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        
+        List<Integer> ans = new ArrayList<Integer>();
+
+        if(root==null) return ans;
+       
+            Stack<TreeNode> stack = new Stack<TreeNode>();
+
+            stack.push(root);
+
+            while(!stack.isEmpty()){
+
+                TreeNode curr = stack.pop();
+
+                ans.add(curr.val);
+
+                if(curr.right!=null)
+                stack.push(curr.right);
+                if(curr.left!=null)
+                stack.push(curr.left);
+            }
+
+        return ans;
+    }
 }
